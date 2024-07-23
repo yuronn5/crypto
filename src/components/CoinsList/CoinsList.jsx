49a4da80d9b50d31caf10a2ea00e8d42a82096ec
yurii bilous <yurii.b@elogic.com.ua>
2024-07-23 15:18:19 +0200

    added image

--- src/components/CoinsList/CoinsList.jsx
+++ src/components/CoinsList/CoinsList.jsx
@@ -7,7 +7,24 @@ const CoinsList = ({ coins }) => {
   return (
     <ul className="coins-list">
       {coins.map((coin) => {
-        return <li key={coin.uuid}>{coin.name}</li>
+        return <li key={coin.uuid}>
+          <div className="coin-item__info">
+              <img
+                className="coin-item__logo"
+                src={coin.iconUrl}
+                alt={coin.name}
+              />
+              <p style={{ color: coin.color }}>{coin.name}/USD</p>
+            </div>
+            <div className="coin-item__price">
+              <p style={{ color: coin.color }}>
+                {(+coin.price).toFixed(2)} USD
+              </p>
+              <p style={{ color: coin.color }}>
+                {(+coin.btcPrice).toFixed(2)} BTC
+              </p>
+            </div>
+        </li>
       })}
     </ul>
   );
